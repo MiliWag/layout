23eb8946e597b4ecfb8bbeb22259f199a54df67a
MiliWag <56554686+MiliWag@users.noreply.github.com>
2020-02-20 23:27:55 +0100

    js-try

--- temp/functions.js
+++ temp/functions.js
@@ -644,7 +644,28 @@ function hpSliderButtonSize() {
     $(".hp-slider-button").removeClass("btn-small");
     $(".hp-slider-button").addClass("btn-large");
   }
-} //Document ready
+}
+
+(function () {
+  var navbarToggler = document.querySelector('#js-navbarToggler');
+  var navbar = document.querySelector('#js-navbar');
+  console.log('tlacitko', navbarToggler);
+  console.log('navigace', navbar);
+  navbarToggler.addEventListener('click', function (e) {
+    navbar.classList.toggle('menu-show');
+    e.stopPropagation();
+    console.log('kliknuto');
+  });
+  document.addEventListener('click', function () {
+    navbar.classList.remove('menu-show');
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27) {
+      // 27 je escape
+      navbar.classList.remove('menu-show');
+    }
+  });
+})(); //Document ready
 
 
 $(function () {
