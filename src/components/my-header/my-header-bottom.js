f29295ab8ae3431339de70b6a3b708b4c3cc0084
MiliWag <56554686+MiliWag@users.noreply.github.com>
2020-03-02 22:34:50 +0100

    hamburger-menu

--- src/components/my-header/my-header-bottom.js
+++ src/components/my-header/my-header-bottom.js
@@ -1,35 +1,11 @@
-/*(function() {
-
-    let navbarToggler = document.querySelector('#navbarToggler')
-    let navbar = document.querySelector('#navbar')
-    console.log('tlacitko', navbarToggler)
-    console.log('navigace', navbar)
-
-    navbarToggler.addEventListener('click', function(e) {
-        navbar.classList.toggle('menu-show')
-        e.stopPropagation()
-        console.log('kliknuto')
-    })
-
-    document.addEventListener('click', function() {
-        navbar.classList.remove('menu-show')
-    })
-
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode === 27) {
-            // 27 je escape
-            navbar.classList.remove('menu-show')
-        }
-    })
-})()*/
-
 function myFunction(x) {
-    x.classList.toggle("change")
 
     let navbarToggler = document.querySelector('#navbarToggler')
     let navbar = document.querySelector('#navbar')
 
+
     navbarToggler.addEventListener('click', function(e) {
+        x.classList.toggle('change')
         navbar.classList.toggle('menu-show')
         e.stopPropagation()
         console.log('kliknuto')
@@ -37,6 +13,7 @@ function myFunction(x) {
 
     document.addEventListener('click', function() {
         navbar.classList.remove('menu-show')
+
     })
 
 }
--- temp/functions.js
+++ temp/functions.js
@@ -645,37 +645,12 @@ function hpSliderButtonSize() {
     $(".hp-slider-button").addClass("btn-large");
   }
 }
-/*(function() {
-
-    let navbarToggler = document.querySelector('#navbarToggler')
-    let navbar = document.querySelector('#navbar')
-    console.log('tlacitko', navbarToggler)
-    console.log('navigace', navbar)
-
-    navbarToggler.addEventListener('click', function(e) {
-        navbar.classList.toggle('menu-show')
-        e.stopPropagation()
-        console.log('kliknuto')
-    })
-
-    document.addEventListener('click', function() {
-        navbar.classList.remove('menu-show')
-    })
-
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode === 27) {
-            // 27 je escape
-            navbar.classList.remove('menu-show')
-        }
-    })
-})()*/
-
 
 function myFunction(x) {
-  x.classList.toggle("change");
   var navbarToggler = document.querySelector('#navbarToggler');
   var navbar = document.querySelector('#navbar');
   navbarToggler.addEventListener('click', function (e) {
+    x.classList.toggle('change');
     navbar.classList.toggle('menu-show');
     e.stopPropagation();
     console.log('kliknuto');
